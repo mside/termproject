b8272e97024cd664c3ce89c7f5b2b5c3f37ca3d6
mside <michaelside0@gmail.com>
2017-05-29 17:34:23 -0700

    first

---
+++ public_html/index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>VANARTS STUDENT MOCK PROJECT</title>
+	<meta name="description" content="This is a student exercise website for the Vancouver Institute of Media Arts (www.vanarts.com)">
+	<link rel="icon" type="image/png"  href="favicon.png">
+<!-- CSS -->
+	<link href="css/foundation/foundation.css" rel="stylesheet" type="text/css">
+  <link href="css/foundation/normalize.css" rel="stylesheet" type="text/css">
+	<link href="css/main.css" media="screen, projector, print" rel="stylesheet" type="text/css">
+<!-- modernizr -->
+  <script src="js/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+	<div class="navbar">
+		<label for="show-menu" class="show-menu">Show Menu</label>
+		<input type="checkbox" id="show-menu" role="button">
+		<div class="centered-nav">
+			<div class="nav-logo-container">
+				<a class="nav-logo-link" href="#">
+					<img class="nav-logo" src="img/logo-final.png" alt="Mauro Pinedo VFX logo">
+				</a>
+			</div>
+			<ul id="menu">
+				<li><a class="nav-link" href="#">Home</a></li>
+				<li><a class="nav-link" href="#">Portfolio</a></li>
+				<li><a class="nav-link" href="#">Blog</a></li>
+				<li><a class="nav-link" href="#">About</a></li>
+				<li><a class="nav-link" href="#">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+
+</body>
+</html>
